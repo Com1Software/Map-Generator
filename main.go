23dedc119b27a3c65fa3f9324c2844655cb5a22e
Com1 Software, Inc <information@com1software.com>
2022-11-05 14:28:27 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"runtime"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	fmt.Printf("C1D0U484 Map-Generator - (c) Copyright Com1Software 1992-2022\n")
+	fmt.Printf("Repository : github.com/Com1Software/Map-Generator\n")
+	fmt.Printf("Operating System : %s\n", runtime.GOOS)
+	exefile := "/C1D0U484/C1D0U484.EXE"
+
+	if CheckForFile(exefile) {
+		fmt.Printf("- Parser Detected")
+	}
+
+}
+
+func CheckForFile(path string) bool {
+	file, err := os.Open(path)
+	if err != nil {
+		return false
+	}
+	file.Close()
+	return true
 }
